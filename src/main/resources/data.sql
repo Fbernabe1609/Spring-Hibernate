1a60439d22062de094f6ed038ce07c393d93e580
Fbernabe1609 <fbernabe160903@gmail.com>
2022-11-27 23:37:54 +0100

    Avance

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,7 +1,15 @@
 insert into Student (name,age,DNI) values ( 'Pepe', 20, '1234567E' );
 insert into Student (name,age,DNI) values ( 'Pepa', 19, '1234567A' );
-insert into Student (name,age,DNI) values ( 'Pepo', 21, '1234567I' );
+insert into Student (name,age,DNI) values ( 'Fran', 29, '1234567O' );
+insert into Student (name,age,DNI) values ( 'José', 28, '1234567P' );
+insert into Student (name,age,DNI) values ( 'María', 24, '1234569I' );
+insert into Student (name,age,DNI) values ( 'Pepito', 61, '1239567I' );
+insert into Student (name,age,DNI) values ( 'Pepita', 71, '1238567I' );
+insert into Student (name,age,DNI) values ( 'Alex', 81, '1234564I' );
+insert into Student (name,age,DNI) values ( 'Julio', 11, '1274567I' );
+insert into Student (name,age,DNI) values ( 'Julia', 13, '1274567L' );
 insert into Subject values (1,'Mates',2);
 insert into Subject values (2,'Lengua',2);
 insert into Subject values (3,'Historia',2);
-insert into Enrollments (StudentID,SubjectID) values (1,2),(1,3),(2,1),(2,3),(3,1),(3,2);
+insert into Enrollments (StudentID,SubjectID) values (1,2),(1,3),(2,1),(2,3),(3,1),(3,2),(4,2),(4,3),(5,1),(5,3),(6,1),(6,2),
+                                                     (7,2),(7,3),(8,1),(8,3),(9,1),(9,2), (10,2),(10,3);
